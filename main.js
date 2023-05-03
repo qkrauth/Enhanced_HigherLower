a237f10807ddd8afa9b0436bb11975fae16b591f
Quinten Krauth <quintenkrauth19@gmail.com>
2023-05-03 15:28:34 -0400

    finishing up

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ let number, input;
 
 while (!valid_num) {
     input = window.prompt("Enter a number:");
-    number = Number(input);
+    number = Number(Math.round(input));
 
     if (number != NaN && number > 0) {
         valid_num = true;
     }
+
+    console.log(number);
 }
 
 const num = Math.floor(Math.random() * number) + 1;
@@ -22,13 +24,39 @@ function doGuess() {
 
     console.log(guess);
 
-    if(guess == num) {
-        message.innerHTML = "Correct!";
+    if (guess <= 0 || guess > number) {
+        message.innerHTML = `Out of range. Keep your guesses between 1 and ${number}`
+        return;
     }
+
+    if (!totalGuesses.includes(guess)) {
+        totalGuesses.push(guess)
+    } else {
+        message.innerHTML = "You already guessed this number, try again"
+        return;
+    }
+
+    if (guess == num) {
+        message.innerHTML = `Correct! It took you ${totalGuesses.length} tries. The numbers you guessed were ${totalGuesses}`
+    }
+
     else if (guess > num) {
         message.innerHTML = "Too high!"
+
+        if (!totalGuesses.includes(guess)) {
+            totalGuesses.push(guess)
+        }
     }
-    else {
+
+    else if (guess < num) {
         message.innerHTML = "Too low!"
+
+        if (!totalGuesses.includes(guess)) {
+            totalGuesses.push(guess)
+        }
+    }
+
+    else {
+        message.innerHTML = "Try again"
     }
 };
